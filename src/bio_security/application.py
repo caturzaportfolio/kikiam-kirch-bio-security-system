2a8baa5a658c88f6eeaede599709294c31f7a92c
Kirch Ivan Balite <kirchbalite.careers@gmail.com>
2026-09-12 20:24:19 +0800

    Kikiam: satisfy Ruff import modernization

--- src/bio_security/application.py
+++ src/bio_security/application.py
@@ -2,9 +2,9 @@
 
 from __future__ import annotations
 
+from collections.abc import Callable
 from dataclasses import dataclass
 from time import perf_counter
-from typing import Callable
 
 from bio_security.domain import BoundingBox, TimingAccumulator, TimingSample
 from bio_security.ports import FaceDetector, Frame, FrameSource
